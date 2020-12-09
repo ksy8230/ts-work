99e09e47d198ddf62354a64f307a6a0882daabca
ksy <ksy8230@hanmail.net>
2020-12-09 19:57:17 +0900

    async await 구문

--- ch01/src/async/index.ts
+++ ch01/src/async/index.ts
@@ -1,5 +1,4 @@
 import { readFileSync, readFile } from 'fs';
-import { reject } from 'ramda';
 
 const Async = () => {
 
@@ -10,7 +9,9 @@ const Async = () => {
     });
     // Promise와 async/await로 읽어들이기
     const readFilePromise = (filename: string):Promise<string> => {
-        return new Promise<string>((resolve, reject) => {
+        return new Promise<string>(
+            (resolve: (successValue: string) => void, 
+            reject: (any) => void) => {
             readFile(filename, (error: Error | null, buffer: Buffer) => {
                 if(error) {
                     reject(error);
@@ -25,6 +26,23 @@ const Async = () => {
         console.log('Promise와 async/await로 읽어들이기', content);
     }
     test();
+    // async & await 구문
+    const asyncTest1 = async () => {
+        let value = await 1;
+        console.log(value);
+        value = await Promise.resolve(1);
+        console.log(value);
+    }
+    const asyncTest2 = async () => {
+        let value = await 'hello';
+        console.log(value);
+        value = await Promise.resolve('hello');
+        console.log(value);
+    }
+    // asyncTest1()
+    // asyncTest2()
+    console.log('Promise 객체로 사용해 보기')
+    asyncTest1().then(() => asyncTest2())
 
 };
 
