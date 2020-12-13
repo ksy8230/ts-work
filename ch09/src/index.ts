4557989c2ed30613e3b22e5b06061961f06440ee
ksy <ksy8230@hanmail.net>
2020-12-13 10:58:37 +0900

    람다 - R.lte, R.gt, R.allPass, R.not, R.ifElse"

--- ch09/src/index.ts
+++ ch09/src/index.ts
@@ -1,7 +1,9 @@
 import { Dump } from "./dump";
 import Range from "./range";
 import {Add_Subtract_Multiply_Divide} from "./add";
+import Lte_gt from "./lte-gt";
 
 Range();
 Dump();
-Add_Subtract_Multiply_Divide()
+Add_Subtract_Multiply_Divide();
+Lte_gt();
