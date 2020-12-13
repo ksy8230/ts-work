cf61d6f33827173beea3d4a7b2d15104cba4b36f
ksy <ksy8230@hanmail.net>
2020-12-13 11:27:23 +0900

    람다 - 문자열 다루기

--- ch09/src/index.ts
+++ ch09/src/index.ts
@@ -2,8 +2,10 @@ import { Dump } from "./dump";
 import Range from "./range";
 import {Add_Subtract_Multiply_Divide} from "./add";
 import Lte_gt from "./lte-gt";
+import Trim_tolowe_split_toCamelCase from "./trim_tolowe_split_toCamelCase";
 
 Range();
 Dump();
 Add_Subtract_Multiply_Divide();
 Lte_gt();
+Trim_tolowe_split_toCamelCase();
