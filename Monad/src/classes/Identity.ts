aafc5de1f40504eebb926a38455974a0bf57ee9d
ksy <ksy8230@hanmail.net>
2020-12-28 10:32:23 +0900

    모나드 - Identity 구현 2: IApply, IChain 추가

--- Monad/src/classes/Identity.ts
+++ Monad/src/classes/Identity.ts
@@ -1,8 +1,15 @@
+import { IChain } from "../interfaces/IChain";
+import { IApply } from "../interfaces/IApply";
 import { IFunctor } from "../interfaces/IFunctor";
 import { ISetoid } from "../interfaces/ISetoid";
 
-export class Identity<T> implements ISetoid<T>, IFunctor<T> {
+export class Identity<T> implements ISetoid<T>, IFunctor<T>, IApply<T>, IChain<T> {
     constructor(private _value: T) {}
+
+    static of<T>(value: T): Identity<T> { 
+        return new Identity<T>(value)
+    }
+
     // IValuable
     value() {
         return this._value;
@@ -19,4 +26,15 @@ export class Identity<T> implements ISetoid<T>, IFunctor<T> {
     map<U>(fn: (x: T) => U) {
         return new Identity<U>(fn(this.value()))
     }
+    // IApply
+    ap<U>(b: U) {
+        const f = this.value() 
+        if(f instanceof Function)
+            return Identity.of<U>((f as Function)(b))
+    }
+    // IChain
+    chain<U>(fn: (T) => U): U {
+        return fn(this.value())
+    }
+    
 }
--- Monad/src/index.ts
+++ Monad/src/index.ts
@@ -2,9 +2,19 @@ import { Identity } from "./classes/Identity";
 
 const one = new Identity(1);
 const two = new Identity(2);
+
+const add = x => y => x + y
+const id = new Identity(add)
+
 console.log(
     one.equals(two), // false
     one, // Identity { _value: 1 }
     one.value(), // 1
     one.equals(null)
 )
+// 엔도 펑터 map은 항상 같은 카테고리에 머무르지만
+// chain은 머무르고 싶은 카테고리를 스스로 정해야 한다
+console.log(
+    Identity.of(1).map(value => `this is ${value}`).value(),
+    Identity.of(1).chain(value => Identity.of(`this is ${value}`)).value()
+  )
