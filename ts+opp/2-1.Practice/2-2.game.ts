58b3e8a788c5b723ac4fdc6d4dd5891e673af812
ksy <ksy8230@hanmail.net>
2021-02-06 12:10:09 +0900

    챌린지1 - 간단한 좌표 게임

---
+++ ts+opp/2-1.Practice/2-2.game.ts
@@ -0,0 +1,52 @@
+/**
+ * game
+ */
+type Movename = 'up' | 'down' | 'left' | 'right';
+
+type Point = {
+    x: number;
+    y: number;
+}
+
+const position:Point = {
+    x : 0,
+    y : 0,
+}
+
+/**
+ * 
+ * @param name only 'up' | 'down' | 'left' | 'right';
+ * 
+ */
+
+function move(name:Movename):void | never{
+    switch(name) {
+        case 'up' : 
+            position.y += 1;
+            break;
+        case 'down' : 
+            position.y -= 1;
+            break;
+        case 'left' : 
+            position.x -= 1;
+            break;
+        case 'right' : 
+            position.x += 1;
+            break;
+        default:
+            throw new Error(`unknown: ${name}`)
+    }
+    
+    
+}
+
+
+move('up')
+console.log(position);
+move('down')
+console.log(position);
+move('left')
+console.log(position);
+move('right')
+console.log(position);
+
